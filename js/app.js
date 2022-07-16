921be71ece0c7deb68b84a28a261af6a434fb5ff
Édison Renato Silva <edisonrenato@gmail.com>
2022-07-16 11:11:26 -0300

    with" working

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
-var ViewModel = function (){
+var Cat = function(){
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/cat1.jpg');
   this.imgAttribution = ko.observable('flicker whatever');
+  this.nickname = ["lindo","fofo","momo", "mimi"];
   this.level = ko.computed(function(){
     let mil = this.clickCount() <= 10 ? "Newborn" : this.clickCount() >= 20? "Infant":"Teen";
     return mil;
-  },this);
+  },this)
+
+};
+
+
+
+var ViewModel = function (){
+  var self = this;
+  this.currentCat = ko.observable(new Cat());
   this.incrementCounter = function (){
-    this.clickCount(this.clickCount() + 1);
-  };
-  this.nickname = ["lindo","fofo","momo", "mimi"]
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  }
 
 };
 
