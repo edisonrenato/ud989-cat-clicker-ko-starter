d64c79084eb3387d6324fbbeba1e73088c16fa84
Édison Renato Silva <edisonrenato@gmail.com>
2022-07-16 10:13:50 -0300

    Cat Levels OK

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@ var ViewModel = function (){
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/cat1.jpg');
   this.imgAttribution = ko.observable('flicker whatever');
-
+  this.level = ko.computed(function(){
+    let mil = this.clickCount() <= 10 ? "Newborn" : this.clickCount() >= 20? "Infant":"Teen";
+    return mil;
+  },this);
   this.incrementCounter = function (){
     this.clickCount(this.clickCount() + 1);
   }
